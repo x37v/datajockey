2bd7327d0a7df1c944b82eee50402b581613083f
Alex Norman <x37v.alex@gmail.com>
2009-08-17 16:30:07 -0700

    Scheduler
    created ScheduleNodes which implement a doublely linked list for the
    schedule
    created an Add command which will add this to the schedule

--- include/scheduler.hpp
+++ include/scheduler.hpp
@@ -10,18 +10,55 @@ namespace DataJockey {
 		private:
 			JackCpp::RingBuffer<Command *> mCommandsIn;
 			JackCpp::RingBuffer<Command *> mCommandsOut;
+			class ScheduleNode;
+			class AddCommand;
+			//this is the main schedule, relative to the transport
+			ScheduleNode * mSchedule;
+			//a pointer to the [guessed] current location in the schedule
+			ScheduleNode * mScheduleCur;
 		public:
 			Scheduler();
 			//this executes a command now, scheduler now owns this command
 			void execute(Command * cmd);
 			//this schedules a command based on the main transport
-			void schedule(const TimePoint &time, const Command &cmd);
+			//returns an ID for this node
+			unsigned int schedule(const TimePoint &time, Command * cmd);
+			//removes a scheduled node
+			void remove(unsigned int id);
 			//this clears the schedule [as soon as possible]
 			void clear();
 			//this is called by the audio callback
 			//it executes commands based on the schedule
 			//XXX memory leak, we don't yet clear out mCommandsOut;
 			void execute_schedule(const Transport& transport);
+			//this is called back in the main thread, 
+			//clearing the command out buffer and executing their done actions
+			void execute_done_actions();
+
+			friend class AddCommand;
+		private:
+			//adds a node to the schedule
+			void add(ScheduleNode * node);
+			//removes a node from the schedule
+			void remove(ScheduleNode * node);
+
+			class ScheduleNode {
+				public:
+					ScheduleNode(Command * c, const TimePoint& t);
+					ScheduleNode * next;
+					ScheduleNode * prev;
+					Command * command;
+					TimePoint time;
+			};
+
+			class AddCommand : public Command {
+				public:
+					AddCommand(Scheduler * scheduler, ScheduleNode * node);
+					virtual void execute();
+				private:
+					Scheduler * mScheduler;
+					ScheduleNode * mNode;
+			};
 	};
 }
 
--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -10,6 +10,8 @@ using namespace DataJockey;
 Scheduler::Scheduler() : 
 	mCommandsIn(1024), mCommandsOut(1024)
 {
+	mSchedule = NULL; 
+	mScheduleCur = NULL;
 }
 
 void Scheduler::execute(Command * cmd){
@@ -21,7 +23,15 @@ void Scheduler::execute(Command * cmd){
 	}
 }
 
-void Scheduler::schedule(const TimePoint &time, const Command &cmd){
+unsigned int Scheduler::schedule(const TimePoint &time, Command * cmd){
+	ScheduleNode * node = new ScheduleNode(cmd, time);;
+	AddCommand * addCmd = new AddCommand(this, node);
+	execute(addCmd);
+	//XXX not implemented
+	return 0;
+}
+
+void remove(unsigned int id){
 	//XXX not implemented
 }
 
@@ -39,7 +49,65 @@ void Scheduler::execute_schedule(const Transport& transport){
 		mCommandsIn.read(cmd);
 		cmd->execute();
 		cmd->time_executed(pos);
-		mCommandsOut.write(cmd);
+		if(mCommandsOut.getWriteSpace())
+			mCommandsOut.write(cmd);
+		//XXX what if there is no write space?
 	}
 }
 
+void Scheduler::execute_done_actions(){
+	//while there are commands to be executed
+	//execute them, noteing the time, 
+	//and write them to the out buffer
+	while(mCommandsOut.getReadSpace()){
+		Command * cmd;
+		mCommandsOut.read(cmd);
+		cmd->execute_done();
+	}
+}
+
+void Scheduler::add(ScheduleNode * node){
+	//if this is the first we set the schedule to this node
+	if(mSchedule == NULL)
+		mSchedule = node;
+	else if (mSchedule->time > node->time){
+		//if the first node has a later time than this new node
+		//place this new node at the front
+		node->next = mSchedule;
+		mSchedule->prev = node;
+		mSchedule = node;
+	} else {
+		ScheduleNode * cur = mSchedule;
+		//find our place in the schedule
+		while(cur->next != NULL && cur->next->time < node->time)
+			cur = cur->next;
+		node->prev = cur;
+		node->next = cur->next;
+		cur->next = node;
+		//if there is a next node set its prev to us
+		if(node->next)
+			node->next->prev = node;
+	}
+}
+
+void Scheduler::remove(ScheduleNode * node){
+	if(node->next)
+		node->next->prev = node->prev;
+	if(node->prev)
+		node->prev->next = node->next;
+}
+
+Scheduler::ScheduleNode::ScheduleNode(Command * c, const TimePoint& t){
+	next = prev = NULL;
+	command = c;
+	time = t;
+}
+
+Scheduler::AddCommand::AddCommand(Scheduler * scheduler, ScheduleNode * node){
+	mScheduler = scheduler;
+	mNode = node;
+}
+
+void Scheduler::AddCommand::execute(){
+	mScheduler->add(mNode);
+}
