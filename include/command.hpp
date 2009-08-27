b269df5c11f1022f87c502a01a44babe89dcf39f
Alex Norman <x37v.alex@gmail.com>
2009-08-27 12:49:24 -0700

    Revert "CommandFactory"
    This reverts commit da6a7527d3bab32b49d63fe8b202e03b41fd0361.

--- include/command.hpp
+++ include/command.hpp
@@ -19,25 +19,4 @@ namespace DataJockey {
 	};
 }
 
-/*
-#include <string>
-#include <map>
-#include <boost/pool/detail/singleton.hpp>
-
-namespace DataJockey {
-	class CommandFactory : public boost::details::pool::singleton_default<CommandFactory> {
-		public:
-			typedef std::string identifier_t;
-			typedef std::string arg_list_t;
-			typedef Command* (*create_command_callback)(arg_list_t);
-			bool register_command(const identifier_t& id, create_command_callback cb);
-			Command* create_command(identifier_t t, arg_list_t args);
-		private:
-			typedef std::map<identifier_t, create_command_callback> callback_map_t;
-		private:
-			callback_map_t mCallbacks;
-	};
-}
-*/
-
 #endif
