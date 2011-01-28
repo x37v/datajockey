3609282b04350aee9456ea560f076fd68940d2ab
Alex Norman <x37v.alex@gmail.com>
2011-01-27 21:17:41 -0800

    started doubly linked list, updated beat buffer a bunch
    the beat buffer will now simply house and not parse beat info..

--- audio/include/beatbuffer.hpp
+++ audio/include/beatbuffer.hpp
@@ -4,29 +4,49 @@
 #include "timepoint.hpp"
 #include <string>
 #include <stdexcept>
-#include <vector>
+#include <deque>
 
 namespace DataJockey {
    namespace Audio {
       //XXX assuming 4/4 for now
       class BeatBuffer {
          public:
-            BeatBuffer(std::string dataLocation) throw(std::runtime_error);
+            typedef std::deque<double> beat_list_t;
+            typedef beat_list_t::const_iterator const_iterator;
+            typedef beat_list_t::iterator iterator;
+
+            BeatBuffer();
             double time_at_position(const TimePoint& position) const;
             TimePoint position_at_time(double seconds) const;
             //this one lets us give a 'last position' helper so we don't have to
             //search the whole thing
             TimePoint position_at_time(double seconds, const TimePoint& lastPos) const;
-            TimePoint end() const;
+            TimePoint end_position() const;
             unsigned int start_offset() const;
-            //setters
+
+            BeatBuffer::const_iterator begin() const;
+            BeatBuffer::iterator begin();
+            BeatBuffer::const_iterator end() const;
+            BeatBuffer::iterator end();
+
+            unsigned int length() const;
+
+            //setters realtime safe
             void start_offset(unsigned int b);
+
+            //update the value of the beat at the index given
+            void update_value(unsigned int index, double value);
+            void update_value(iterator pos, double value);
+
+            //setters not realtime safe
+            void clear();
+            void insert_beat(double seconds);
+
          private:
-            void load(std::string dataLocation) throw(std::runtime_error);
             //the start beat lets us offset the data
             //indicating where bar 0, beat 0 should in the data
             unsigned int mStartBeat;
-            std::vector<double> mBeatData;
+            beat_list_t mBeatData;
       };
    }
 }
--- audio/src/beatbuffer.cpp
+++ audio/src/beatbuffer.cpp
@@ -1,6 +1,4 @@
 #include "beatbuffer.hpp"
-//#include <yamlcpp/yaml.hpp>
-//#include <yamlcpp/parser.hpp>
 
 using namespace DataJockey::Audio;
 
@@ -9,38 +7,36 @@ Type linear_interp(Type v0, Type v1, double dist){
    return v0 + (v1 - v0) * dist;
 }
 
-BeatBuffer::BeatBuffer(std::string dataLocation) 
-   throw(std::runtime_error){
-      mStartBeat = 0;
-      load(dataLocation);
-   }
+BeatBuffer::BeatBuffer() {
+   mStartBeat = 0;
+}
 
 //XXX assuming 4/4
-   double BeatBuffer::time_at_position(const TimePoint& position) const {
-      if(TimePoint::SECONDS == position.type())
-         return position.seconds();
-      else {
-         if(position.valid()){
-            int beat = position.beat() + position.bar() * 4;
-            unsigned int size = mBeatData.size();
-            beat += mStartBeat;
-            //make sure we're in range!
-            if (size == 0)
-               return 0.0;
-            else if(beat < 0)
-               return 0.0;
-            else if((unsigned int)beat >= size)
-               return mBeatData.back();
-            else if (position.pos_in_beat() <= 0 || 
-                  (position.pos_in_beat() > 0 && (unsigned int)beat + 1 >= size))
-               return mBeatData[beat];
-            else
-               return linear_interp(mBeatData[beat], 
-                     mBeatData[beat + 1], position.pos_in_beat());
-         } else
+double BeatBuffer::time_at_position(const TimePoint& position) const {
+   if(TimePoint::SECONDS == position.type())
+      return position.seconds();
+   else {
+      if(position.valid()){
+         int beat = position.beat() + position.bar() * 4;
+         unsigned int size = mBeatData.size();
+         beat += mStartBeat;
+         //make sure we're in range!
+         if (size == 0)
             return 0.0;
-      }
+         else if(beat < 0)
+            return 0.0;
+         else if((unsigned int)beat >= size)
+            return mBeatData.back();
+         else if (position.pos_in_beat() <= 0 || 
+               (position.pos_in_beat() > 0 && (unsigned int)beat + 1 >= size))
+            return mBeatData[beat];
+         else
+            return linear_interp(mBeatData[beat], 
+                  mBeatData[beat + 1], position.pos_in_beat());
+      } else
+         return 0.0;
    }
+}
 
 TimePoint BeatBuffer::position_at_time(double seconds) const {
    TimePoint pos;
@@ -74,12 +70,12 @@ TimePoint BeatBuffer::position_at_time(double seconds, const TimePoint& lastPos)
          }
       }
       //XXX wasn't found.. should return end or invalid?
-      return end();
+      return end_position();
    }
    return pos;
 }
 
-TimePoint BeatBuffer::end() const {
+TimePoint BeatBuffer::end_position() const {
    TimePoint pos;
    unsigned int beats;
    if(mBeatData.size() == 0)
@@ -91,101 +87,100 @@ TimePoint BeatBuffer::end() const {
    return pos;
 }
 
-void BeatBuffer::load(std::string /*dataLocation*/) throw(std::runtime_error)
-{
-   mBeatData.clear();
-#if 0
-
-   //read in the beat location information
-   try {
-      yaml::Parser y;
-      yaml::node top = y.parseFile(dataLocation);
-      yaml::map_ptr beatLocs = yaml::get<yaml::map_ptr>(top);
-      yaml::map::iterator it = beatLocs->find(std::string("beat locations"));
-      if(it == beatLocs->end())
-         throw std::runtime_error("cannot find beat locations");
-
-      //if it is a sequence use the last one for now!
-      if(yaml::is<yaml::seq_ptr>(it->second)){
-         yaml::seq_ptr beatLocSeq = yaml::get<yaml::seq_ptr>(it->second);
-
-         //store the last
-         beatLocs = yaml::get<yaml::map_ptr>( beatLocSeq->back());
-
-         /*
-         //this tries to find the one with the most recent mtime..
-         beatLocs = yaml::get<yaml::map_ptr>( beatLocSeq->front());
-         try {
-         //do we have to check the first element?
-         for(yaml::seq::iterator seq_it = beatLocSeq->begin();
-         seq_it != beatLocSeq->end(); seq_it++){
-         //grab the map
-         yaml::map_ptr curBeats = yaml::get<yaml::map_ptr>(*seq_it);
-         yaml::map::iterator mtime0 = beatLocs->find(std::string("mtime"));
-         yaml::map::iterator mtime1 = curBeats->find(std::string("mtime"));;
-         //make sure these are valid
-         if(mtime0 != beatLocs->end() && mtime1 != curBeats->end()){
-         //grab the times
-         DateTimePtr t0 = yaml::get<DateTimePtr>(mtime0->second);
-         DateTimePtr t1 = yaml::get<DateTimePtr>(mtime1->second);
-         //if the beatlocs we're using was modified before the current beat locs, 
-         //use the current beat locs
-         if(*t0 < *t1)
-         beatLocs = curBeats;
-         }
-         }
-         } catch (...){
-         //do nothing we've still got the first one set
-         }
-         */
-
-      } else 
-         beatLocs = yaml::get<yaml::map_ptr>(it->second);
-
-      //grab the time point vector
-      it = beatLocs->find(std::string("time points"));
-      if(it == beatLocs->end())
-         throw std::runtime_error("cannot find time points");
-
-      //copy over the data
-      mBeatData = yaml::getVector<double>(it->second);
-   } catch (...){
-      std::string str("error reading beat location file: ");
-      str.append(dataLocation);
-      throw std::runtime_error(str);
-   }
-#endif
-}
-
 unsigned int BeatBuffer::start_offset() const {
    return mStartBeat;
 }
 
-   void BeatBuffer::start_offset(unsigned int val){
-      if(val < mBeatData.size())
-         mStartBeat = val;
+void BeatBuffer::start_offset(unsigned int val){
+   if(val < mBeatData.size())
+      mStartBeat = val;
+}
+
+BeatBuffer::const_iterator BeatBuffer::begin() const { return mBeatData.begin(); }
+BeatBuffer::iterator BeatBuffer::begin() { return mBeatData.begin(); }
+BeatBuffer::const_iterator BeatBuffer::end() const { return mBeatData.end(); }
+BeatBuffer::iterator BeatBuffer::end() { return mBeatData.end(); }
+
+unsigned int BeatBuffer::length() const { return mBeatData.size(); }
+
+void BeatBuffer::update_value(unsigned int index, double value) {
+   if(index < mBeatData.size()) {
+      mBeatData[index] = value;
    }
+}
 
+void BeatBuffer::update_value(iterator pos, double value) {
+   if (pos != end())
+      *pos = value;
+}
 
-/*
+void BeatBuffer::clear() {
+   mBeatData.clear();
+}
+
+void BeatBuffer::insert_beat(double seconds) {
+   //if it is empty or our new time point is greater than the last time point
+   if (mBeatData.empty() || mBeatData.back() < seconds)
+      mBeatData.push_back(seconds);
+   else if(mBeatData.front() > seconds){
+      mBeatData.push_front(seconds);
+   } else {
+      beat_list_t::iterator it = mBeatData.begin();
+      //search for the position where the value is not less than our value to insert
+      while(*it < seconds)
+         it++;
+      //insert inserts before the iterator
+      mBeatData.insert(it, seconds);
+   }
+}
+
+#if 0
+
 #include <iostream>
 using std::cout;
 using std::endl;
 
-int main(int argc, char * argv[]){
-TimePoint t;
-t.at_bar(1);
-
-BeatBuffer b(argv[1]);
-
-cout << b.time_at_position(t) << endl;
-cout << b.end().bar() << " : " << b.end().beat() << endl;
-
-t = b.position_at_time(6.75490263682522 );
-cout << "position : " << endl;
-cout << t.bar() << " : " << t.beat() << " : " << t.pos_in_beat() << endl;
-
-return 0;
+void printme(const BeatBuffer& buff) {
+   for(BeatBuffer::const_iterator it = buff.begin(); it != buff.end(); it++) {
+      cout << *it << endl;
+   }
 }
 
-*/
+int main(int argc, char * argv[]){
+
+   BeatBuffer b;
+   TimePoint t(1,0,0);
+
+   b.insert_beat(2.0);
+   printme(b);
+
+   b.insert_beat(4.0);
+   b.insert_beat(5.0);
+   b.insert_beat(6.0);
+   b.insert_beat(7.0);
+
+   cout << "printing" << endl;
+   printme(b);
+
+   BeatBuffer::iterator it = b.begin();
+   *it = 0.1;
+   cout << *it << endl;
+
+   b.insert_beat(2.0);
+   b.insert_beat(1.0);
+   cout << "printing 2" << endl;
+   printme(b);
+   cout << "asdf" << endl;
+
+   cout << b.time_at_position(t) << endl;
+   cout << b.end_position().bar() << " : " << b.end_position().beat() << endl;
+
+   t = b.position_at_time(6.75490263682522 );
+   cout << "position : " << endl;
+   cout << t.bar() << " : " << t.beat() << " : " << t.pos_in_beat() << endl;
+
+   return 0;
+}
+
+#endif
+
