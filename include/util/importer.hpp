3d5bbae23f6064c04e39e13f9190fce1446d4f15
Alex Norman <x37v.alex@gmail.com>
2012-06-05 11:12:04 -0700

    updated todos

--- include/util/importer.hpp
+++ include/util/importer.hpp
@@ -15,6 +15,7 @@ namespace dj {
          public:
             Importer(QObject * parent = NULL);
          public slots:
+            //TODO, extract tempo from beat locations
             void import(const QStringList& file_list, bool recurse_directories = true);
             void import_blocking(const QString& audio_file);
          signals:
