49da4b44069ed882ac26eae7ed11f308fab4ee8f
Alex Norman <x37v.alex@gmail.com>
2012-11-24 09:05:47 -0800

    responding to quit for script, using exec in the new process

--- ruby/scripts/record_with_score.rb
+++ ruby/scripts/record_with_score.rb
@@ -9,26 +9,29 @@ SERVER_PORT = 10010
 jack_ports = ['datajockey:output0', 'datajockey:output1']
 audio_file_out = "datajockey_capture-#{@@start_time.to_s.gsub(" ", "_")}.wav"
 
-rec_process = fork do
-  system('jack_capture', '-dc', '-c', jack_ports.size.to_s, '-fn', audio_file_out, *jack_ports.collect { |p| ['--port', p] }.flatten)
-  Kernel.exit!
+@@rec_process = fork do
+  exec('jack_capture', '-dc', '-c', jack_ports.size.to_s, '-fn', audio_file_out, *jack_ports.collect { |p| ['--port', p] }.flatten)
 end
-
-trap("SIGINT") {
-  puts "sending hup"
-  Process.kill("HUP", rec_process)
-  Process.wait
-  exit
-}
 
 def evt_time
   Time.now - @@start_time
+end
+
+def quit_app
+  Process.kill("TERM", @@rec_process)
+  Process.wait
+  exit
+end
+
+trap("SIGINT") do
+  quit_app
 end
 
 puts "##{@@start_time} #{audio_file_out}"
 
 OSC.run do
   server = OSC::Server.new SERVER_PORT
+
   server.add_pattern %r{/dj/player/.*} do |*args| # this will match any /foo node
     player = args[0].split('/')[3].to_i
     cmd = args[0].sub(%r{/dj/player/\d/}, "").gsub("/", "_")
@@ -42,5 +45,9 @@ OSC.run do
     when 'load'
       puts "#{evt_time} #{player} load #{args[1]}" if args.size == 2
     end
+  end
+
+  server.add_pattern '/dj/quit' do |*args| 
+    quit_app
   end
 end
