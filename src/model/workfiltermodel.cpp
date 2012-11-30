900102a92d710144e3e3f8e39aea107346ebd178
Alex Norman <x37v.alex@gmail.com>
2012-11-29 22:21:09 -0800

    working more with expressions

--- src/model/workfiltermodel.cpp
+++ src/model/workfiltermodel.cpp
@@ -48,24 +48,86 @@ namespace {
     return expression;
   }
 
+  const QRegExp current_bpm_with_mul(
+    "\\((?:cbpm|current_tempo|current_bpm)\\s*(\\d+\\.?\\d*%?)\\s*\\)",
+      Qt::CaseInsensitive);
+
+  const QRegExp current_bpm_regex(
+    "cbpm|current_tempo|current_bpm",
+      Qt::CaseInsensitive);
+
+  //(bpm x,y) -> ((bpm >= x) and (bpm <= y)) 
+  const QRegExp bpm_range_regex(
+    "\\((?:bpm|tempo_median|tempo)\\s*([\\w\\.\\d]+)\\s*,\\s*([\\w\\.\\d]+)\\s*\\)",
+      Qt::CaseInsensitive);
+
+  //gt/lt/eq..
   const QRegExp bpm_reg(
       "\\((?:bpm|tempo_median|tempo)\\s*((?:>=|<=|=|>|<)\\s*[\\d\\.]+)\\s*\\)",
       Qt::CaseInsensitive);
 
-  QString replace_tempo_median(QString expression) throw(std::runtime_error) {
-    QRegExp rx(bpm_reg);
-    rx.setMinimal(true);
+  QString replace_tempo_median(QString expression, double current_tempo) throw(std::runtime_error) {
+    QRegExp rx;
     int pos = 0;
+
+    //current_bpm_with_mul
+    rx = QRegExp(current_bpm_with_mul);
+    rx.setMinimal(true);
+    while((pos = rx.indexIn(expression, 0)) != -1) {
+      const int len = rx.matchedLength();
+      QStringList captures = rx.capturedTexts();
+      QString mul = captures[1];
+
+      float value = 0;
+      if (mul.contains("%")) {
+        mul.remove("%");
+        value = mul.toFloat() / 100.0f;
+      } else {
+        value = mul.toFloat();
+      }
+
+      QString bottom, top;
+      bottom.setNum(current_tempo * (1.0 - value));
+      top.setNum(current_tempo * (1.0 + value));
+
+      QString replacement = "((tempo_median >= " + bottom + ") and (tempo_median <= " + top + "))";
+      expression.replace(pos, len, replacement);
+    }
+
+    //current_bpm
+    rx = QRegExp(current_bpm_regex);
+    rx.setMinimal(true);
+    QString current_tempo_s;
+    current_tempo_s.setNum(current_tempo);
+    while((pos = rx.indexIn(expression, 0)) != -1) {
+      const int len = rx.matchedLength();
+      expression.replace(pos, len, current_tempo_s);
+    }
+
+    //ranges
+    rx = QRegExp(bpm_range_regex);
+    rx.setMinimal(true);
+    while((pos = rx.indexIn(expression, 0)) != -1) {
+      const int len = rx.matchedLength();
+      QStringList captures = rx.capturedTexts();
+      QString replacement = "((tempo_median >= " + captures[1] + ") and (tempo_median <= " + captures[2] + "))";
+      expression.replace(pos, len, replacement);
+    }
+
+    //gt/lt..
+    rx = QRegExp(bpm_reg);
+    rx.setMinimal(true);
     while((pos = rx.indexIn(expression, 0)) != -1) {
       const int len = rx.matchedLength();
       expression.replace(pos, len, "works.tempo_median " + rx.capturedTexts()[1]);
     }
+
     return expression;
   }
 
-  QString filter_to_sql(QString expression) throw(std::runtime_error) {
+  QString filter_to_sql(QString expression, double current_tempo) throw(std::runtime_error) {
     expression = replace_tag_expressions(expression);
-    expression = replace_tempo_median(expression);
+    expression = replace_tempo_median(expression, current_tempo);
     return expression;
   }
 }
@@ -79,7 +141,8 @@ void WorkFilterModel::set_filter_expression(QString expression) {
 
   mFilterExpression = expression;
   try {
-    QString sql_expr = filter_to_sql(expression);
+    double current_tempo = 100.0; //XXX hardcoded tmp
+    QString sql_expr = filter_to_sql(expression, current_tempo);
     //cout << sql_expr.toStdString() << endl;
     db::work::filtered_update(tableName(), sql_expr);
     select();
