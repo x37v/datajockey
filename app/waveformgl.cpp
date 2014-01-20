1324f8134d49b4c9514671c37a8b34965f543450
Alex Norman <x37v.alex@gmail.com>
2014-01-19 22:13:32 -0800

    waveform computation in another thread
    could use some cleanup probably

--- app/waveformgl.cpp
+++ app/waveformgl.cpp
@@ -1,10 +1,29 @@
 #include "waveformgl.h"
 #include <limits>
 
+namespace {
+  bool registered = false;
+}
+
 WaveFormGL::WaveFormGL(QObject * parent) : QObject(parent)
 {
-  mLines.resize(mWidth);
-  mXStartLast = -mLines.size();
+  mWaveformLines.resize(mWidth);
+  mXStartLast = -mWaveformLines.size();
+  mWaveformCalculator = new WaveformLineCalculator();
+  mCalculateThread = new QThread(this);
+  mWaveformCalculator->moveToThread(mCalculateThread);
+
+  if (!registered) {
+    qRegisterMetaType<gl2triangles_t>("gl2triangles_t");
+    registered = true;
+  }
+
+  connect(mWaveformCalculator, SIGNAL(lineChanged(int, gl2triangles_t)),
+      this, SLOT(setWaveformLine(int, gl2triangles_t)));
+  connect(this, SIGNAL(waveformLinesRequested(djaudio::AudioBufferPtr, int, int, int)),
+      mWaveformCalculator, SLOT(compute(djaudio::AudioBufferPtr, int, int, int)));
+
+  mCalculateThread->start();
 }
 
 int WaveFormGL::frameAtX(GLfloat x) const {
@@ -20,20 +39,22 @@ void WaveFormGL::setAudioBuffer(djaudio::AudioBufferPtr buffer) {
 
   if (mZoomFull)
     mFramesPerLine = mAudioBuffer->length() / mWidth;
-  mXStartLast = -mLines.size();
+  mXStartLast = -mWaveformLines.size();
   updateLines();
 }
-
+ 
 void WaveFormGL::setBeatBuffer(djaudio::BeatBufferPtr buffer) {
-  mBeats.clear();
+  return;
+  mBeatLines.clear();
   if (mZoomFull || !buffer)
     return;
-  mBeats.resize(buffer->size());
+
+  mBeatLines.resize(buffer->size());
   for (unsigned int i = 0; i < buffer->size(); i++) {
     GLfloat x = static_cast<GLfloat>(buffer->at(i)) / mFramesPerLine;
-    mBeats[i].x0 = mBeats[i].x1 = x;
-    mBeats[i].y0 = -1;
-    mBeats[i].y1 = 1;
+    mBeatLines[i].x0 = mBeatLines[i].x1 = x;
+    mBeatLines[i].y0 = -1;
+    mBeatLines[i].y1 = 1;
   }
 }
 
@@ -45,19 +66,19 @@ void WaveFormGL::setPositionFrame(int frame) {
 
 void WaveFormGL::setWidth(int pixels) {
   mWidth = pixels;
-  mLines.resize(mWidth);
-  mXStartLast = -mLines.size();
+  mWaveformLines.resize(mWidth);
+  mXStartLast = -mWaveformLines.size();
   updateLines();
 }
 
 void WaveFormGL::historyWidth(int pixels) {
   mHistoryWidth = pixels;
-  mXStartLast = -mLines.size();
+  mXStartLast = -mWaveformLines.size();
   updateLines();
 }
 
 void WaveFormGL::draw() {
-  if (!mAudioBuffer || mLines.size() == 0)
+  if (!mAudioBuffer || mWaveformLines.size() == 0)
     return;
 
   //draw waveform
@@ -66,18 +87,18 @@ void WaveFormGL::draw() {
   if (!mZoomFull)
     glTranslatef(-((mFramePosition / mFramesPerLine) - mHistoryWidth), 0.0, 0.0);
   glEnableClientState(GL_VERTEX_ARRAY);
-  glVertexPointer(2, GL_FLOAT, 0, &mLines.front());
-  glDrawArrays(GL_TRIANGLES, 0, mLines.size() * 6);
+  glVertexPointer(2, GL_FLOAT, 0, &mWaveformLines.front());
+  glDrawArrays(GL_TRIANGLES, 0, mWaveformLines.size() * 6);
   glDisableClientState(GL_VERTEX_ARRAY);
   
-  if (mBeats.size()) {
+  if (mBeatLines.size()) {
     //draw beats
     glPushMatrix();
     glColor4d(mBeatColor.redF(), mBeatColor.greenF(), mBeatColor.blueF(), mBeatColor.alphaF());
     glLineWidth(1.0);
     glEnableClientState(GL_VERTEX_ARRAY);
-    glVertexPointer(2, GL_FLOAT, 0, &mBeats.front());
-    glDrawArrays(GL_LINES, 0, mBeats.size() * 2);
+    glVertexPointer(2, GL_FLOAT, 0, &mBeatLines.front());
+    glDrawArrays(GL_LINES, 0, mBeatLines.size() * 2);
     glDisableClientState(GL_VERTEX_ARRAY);
     glPopMatrix();
   }
@@ -96,6 +117,15 @@ void WaveFormGL::draw() {
   glEnd();
 }
 
+void WaveFormGL::setBeatLines(QVector<glline_t> lines) {
+  mBeatLines = lines;
+}
+
+void WaveFormGL::setWaveformLine(int lineIndex, gl2triangles_t value) {
+  if (mWaveformLines.size())
+    mWaveformLines[lineIndex % mWaveformLines.size()] = value;
+}
+
 void WaveFormGL::framesPerLine(int v) {
   mFramesPerLine = v;
   updateLines();
@@ -105,27 +135,26 @@ void WaveFormGL::updateLines() {
   if (!mAudioBuffer)
     return;
 
-  int start_line = 0;
+  if (mZoomFull) {
+    emit(waveformLinesRequested(mAudioBuffer, 0, mWaveformLines.size(), mFramesPerLine));
+    return;
+  }
+
   int start_last = mXStartLast;
-  if (!mZoomFull)
-    start_line = std::max((mFramePosition / mFramesPerLine) - mHistoryWidth, 0);
+  int start_line = std::max((mFramePosition / mFramesPerLine) - mHistoryWidth, 0);
   mXStartLast = start_line;
-  int end_line = start_line + mLines.size();
+  int end_line = start_line + mWaveformLines.size();
 
   //figure out whats we need to fill in
   if (start_line > start_last) {
-    int invalid = std::min(start_line - start_last, mLines.size());
+    int invalid = std::min(start_line - start_last, mWaveformLines.size());
     start_line = end_line - invalid;
   } else {
-    int invalid = std::min(start_last - start_line, mLines.size());
+    int invalid = std::min(start_last - start_line, mWaveformLines.size());
     end_line = start_line + invalid;
   }
 
-  for (int i = start_line; i < end_line; i++) {
-    GLfloat height = lineHeight(i);
-    GLfloat x = i;
-    mLines[i % mLines.size()].rect(x, -height, x + 1.0, height);
-  }
+  emit(waveformLinesRequested(mAudioBuffer, start_line, end_line, mFramesPerLine));
 }
 
 GLfloat WaveFormGL::lineHeight(int line_index) const {
@@ -142,5 +171,42 @@ GLfloat WaveFormGL::lineHeight(int line_index) const {
     value = std::max(value, fabsf(mAudioBuffer->sample(1, frame)));
   }
   return (GLfloat)value;
+}
+
+namespace {
+  GLfloat lineHeight(djaudio::AudioBufferPtr buffer, int line_index, int frames_per_line) {
+    //this is only called with a valid audio buffer
+    int start_frame = line_index * frames_per_line;
+
+    if (start_frame < 0 || start_frame >= (int)buffer->length())
+      return (GLfloat)0.0;
+
+    int end_frame = std::min(start_frame + frames_per_line, (int)buffer->length());
+    float value = 0;
+    for (int frame = start_frame; frame < end_frame; frame++) {
+      value = std::max(value, fabsf(buffer->sample(0, frame)));
+      value = std::max(value, fabsf(buffer->sample(1, frame)));
+    }
+    return (GLfloat)value;
+  }
+}
+
+WaveformLineCalculator::WaveformLineCalculator(QObject * parent) :
+  QObject(parent)
+{
+}
+
+void WaveformLineCalculator::compute(djaudio::AudioBufferPtr buffer, int startLine, int endLine, int framesPerLine) {
+  if (!buffer)
+    return;
+
+  for (int i = startLine; i < endLine; i++) {
+    GLfloat height = lineHeight(buffer, i, framesPerLine);
+    GLfloat x = i;
+
+    gl2triangles_t triangle;
+    triangle.rect(x, -height, x + 1.0, height);
+    emit(lineChanged(i, triangle));
+  }
 }
 
--- app/waveformgl.h
+++ app/waveformgl.h
@@ -5,9 +5,36 @@
 #include <QVector>
 #include <QtOpenGL>
 #include <QColor>
+#include <QThread>
 
 #include "audiobuffer.hpp"
 #include "annotation.hpp"
+
+class WaveformLineCalculator;
+
+struct gl2triangles_t{
+  GLfloat x0; GLfloat y0;
+  GLfloat x1; GLfloat y1;
+  GLfloat x2; GLfloat y2;
+  GLfloat x3; GLfloat y3;
+  GLfloat x4; GLfloat y4;
+  GLfloat x5; GLfloat y5;
+  //rect
+  void rect(GLfloat rectx0, GLfloat recty0, GLfloat rectx1, GLfloat recty1) {
+    x0 = rectx0;
+    y0 = recty0;
+
+    x1 = x4 = rectx0;
+    y1 = y4 = recty1;
+
+    x5 = rectx1;
+    y5 = recty1;
+
+    x2 = x3 = rectx1;
+    y2 = y3 = recty0;
+  }
+};
+Q_DECLARE_METATYPE(gl2triangles_t)
 
 //for drawing a waveform within a gl view
 //always draws horizontally, left to right
@@ -32,40 +59,24 @@ class WaveFormGL : public QObject
 
     int frameAtX(GLfloat x) const;
 
+    struct glline_t{
+      GLfloat x0; GLfloat y0;
+      GLfloat x1; GLfloat y1;
+    };
+
   public slots:
     void setAudioBuffer(djaudio::AudioBufferPtr buffer);
     void setBeatBuffer(djaudio::BeatBufferPtr buffer);
     void setPositionFrame(int frame);
     void framesPerLine(int v);
     void draw();
+  signals:
+    void waveformLinesRequested(djaudio::AudioBufferPtr buffer, int startLine, int endLine, int framesPerLine);
+  protected slots:
+    void setBeatLines(QVector<glline_t> lines);
+    void setWaveformLine(int lineIndex, gl2triangles_t value);
 
   private:
-    struct glline_t{
-      GLfloat x0; GLfloat y0;
-      GLfloat x1; GLfloat y1;
-    };
-    struct gl2triangles_t{
-      GLfloat x0; GLfloat y0;
-      GLfloat x1; GLfloat y1;
-      GLfloat x2; GLfloat y2;
-      GLfloat x3; GLfloat y3;
-      GLfloat x4; GLfloat y4;
-      GLfloat x5; GLfloat y5;
-      //rect
-      void rect(GLfloat rectx0, GLfloat recty0, GLfloat rectx1, GLfloat recty1) {
-        x0 = rectx0;
-        y0 = recty0;
-
-        x1 = x4 = rectx0;
-        y1 = y4 = recty1;
-
-        x5 = rectx1;
-        y5 = recty1;
-
-        x2 = x3 = rectx1;
-        y2 = y3 = recty0;
-      }
-    };
     djaudio::AudioBufferPtr mAudioBuffer;
     int mWidth = 800;
     int mHistoryWidth = 100;
@@ -73,15 +84,29 @@ class WaveFormGL : public QObject
     int mFramePosition = 0;
     int mXStartLast;
 
+    QThread * mCalculateThread;
+    WaveformLineCalculator * mWaveformCalculator;
+
     bool mZoomFull = true;
-    QVector<gl2triangles_t> mLines;
-    QVector<glline_t> mBeats;
+    QVector<gl2triangles_t> mWaveformLines;
+    QVector<glline_t> mBeatLines;
     QColor cursorColor = Qt::white;
     QColor mWaveformColor = Qt::red;
     QColor mBeatColor = Qt::yellow;
     void updateLines();
 
     GLfloat lineHeight(int line_index) const;
+};
+
+class WaveformLineCalculator : public QObject {
+  Q_OBJECT
+  public:
+    WaveformLineCalculator(QObject * parent = nullptr);
+    virtual ~WaveformLineCalculator() {}
+  public slots:
+    void compute(djaudio::AudioBufferPtr buffer, int startLine, int endLine, int framesPerLine);
+  signals:
+    void lineChanged(int lineIndex, gl2triangles_t line);
 };
 
 #endif // WAVEFORMGL_H
