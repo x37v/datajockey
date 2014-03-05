769bdac0ef85ce649e33c3204cf2c3f694a678d0
Alex Norman <x37v.alex@gmail.com>
2014-03-04 20:48:49 -0800

    getting recursive on the shit

--- importer/fileprocessor.cpp
+++ importer/fileprocessor.cpp
@@ -1,5 +1,7 @@
 #include "fileprocessor.h"
 #include "audiofileinfoextractor.h"
+#include <QDir>
+#include <QtDebug>
 
 FileProcessor::FileProcessor(QObject *parent) :
   QObject(parent)
@@ -12,8 +14,21 @@ void FileProcessor::addFiles(QStringList files) {
 
 void FileProcessor::process() {
   AudioFileInfoExtractor * extractor = new AudioFileInfoExtractor(this);
-  foreach (QString file, mFiles) {
-    extractor->processAudioFile(file);
+  connect(extractor, &AudioFileInfoExtractor::fileCreated, this, &FileProcessor::fileCreated);
+
+  for(int i = 0; i < mFiles.size(); i++) {
+    QString file = mFiles[i];
+    QDir dir(file);
+    if (dir.exists()) {
+      QStringList entries = dir.entryList(QDir::NoDotAndDotDot | QDir::AllEntries);
+      foreach (QString e, entries)
+        mFiles.append(dir.absoluteFilePath(e));
+      continue;
+    } else if (QFile::exists(file)) {
+      extractor->processAudioFile(file);
+    } else {
+      qDebug() << "doesn't exist " << file << endl;
+    }
   }
   extractor->deleteLater();
   emit(complete());
--- importer/fileprocessor.h
+++ importer/fileprocessor.h
@@ -3,15 +3,18 @@
 
 #include <QObject>
 #include <QStringList>
+#include <QHash>
+#include <QVariant>
 
 class FileProcessor : public QObject
 {
-    Q_OBJECT
+  Q_OBJECT
   public:
     explicit FileProcessor(QObject *parent = 0);
 
   signals:
     void complete();
+    void fileCreated(QString audioFilePath, QString annotationFilePath, QHash<QString, QVariant> tagData);
 
   public slots:
     void addFiles(QStringList files);
