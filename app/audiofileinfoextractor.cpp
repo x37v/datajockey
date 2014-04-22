ab81bf00141e13bb0c23d9e5dbf1b07ea8b23b05
Alex Norman <x37v.alex@gmail.com>
2014-04-21 20:40:32 -0700

    copied over smoothing, untested though

--- app/audiofileinfoextractor.cpp
+++ app/audiofileinfoextractor.cpp
@@ -7,6 +7,34 @@
 #include <QDir>
 #include <QtDebug>
 
+namespace {
+
+  unsigned int smoothing_iterations = 20; //XXX make it configurable
+
+
+  //find the mid point between the previous and next values
+  //find the difference between the data we have and that value, add 1/2 of that to the point
+  unsigned int smoothed_point(unsigned int cur, unsigned int prev, unsigned int next) {
+    double mid = static_cast<double>(next - prev) / 2.0 + static_cast<double>(prev);
+    return static_cast<unsigned int>(cur + (mid - cur) / 2.0);
+  }
+
+  void smooth(std::deque<int>& data, unsigned int iterations) {
+    if (data.empty() || data.size() < 4)
+      return;
+    for (unsigned int i = 0; i < iterations; i++) {
+      //go forward, the backwards
+      if (i % 2 == 0) {
+        for (unsigned int j = 1; j < data.size() - 1; j++)
+          data[j] = smoothed_point(data[j], data[j - 1], data[j + 1]);
+      } else {
+        for (unsigned int j = data.size() - 2; j > 0; j--)
+          data[j] = smoothed_point(data[j], data[j - 1], data[j + 1]);
+      }
+    }
+  }
+}
+
 AudioFileInfoExtractor::AudioFileInfoExtractor(QObject *parent) :
   QObject(parent)
 {
@@ -32,6 +60,9 @@ void AudioFileInfoExtractor::processAudioFile(QString audioFileName) {
     mBeatExtractor->process(audio_buffer, beat_buffer);
 
     std::deque<int> dist = beat_buffer->distances();
+
+    smooth(dist, smoothing_iterations);
+    
     int median = djaudio::median(dist);
     float bpm = (60.0 * audio_buffer->sample_rate()) / median;
     tag_data["tempo_median"] = bpm;
