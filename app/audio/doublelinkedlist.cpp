9943c2ce49a2c7f5e428c35284b1bd0a43d8b5f7
Alex Norman <x37v.alex@gmail.com>
2014-10-11 12:15:31 -0700

    totally changed the double linked list

--- app/audio/doublelinkedlist.cpp
+++ app/audio/doublelinkedlist.cpp
@@ -1,79 +1,64 @@
 #include "doublelinkedlist.hpp"
 #include <stdlib.h>
 
-DoubleLinkedListNode::DoubleLinkedListNode() {
-   mPrev = mNext = NULL;
+DoubleLinkedListNode::DoubleLinkedListNode() { }
+DoubleLinkedListNode::~DoubleLinkedListNode() { }
+
+DoubleLinkedList::DoubleLinkedList() {
 }
 
-DoubleLinkedListNode::~DoubleLinkedListNode() {
-   //we don't remove ourselves from the list because we might just be a copy of
-   //the node
+DoubleLinkedList::~DoubleLinkedList() {
 }
 
-bool DoubleLinkedListNode::first() {
-   return mPrev == NULL;
+void DoubleLinkedList::push_back(DoubleLinkedListNode * node) {
+  node->next = nullptr;
+  node->prev = nullptr;
+  if (!mHead) {
+    mHead = node;
+    return;
+  }
+
+  //find the last element and append node
+  DoubleLinkedListNode * cur = mHead;
+  while (cur->next)
+    cur = cur->next;
+  node->prev = cur;
+  cur->next = node;
 }
 
-bool DoubleLinkedListNode::last() {
-   return mNext == NULL;
+void DoubleLinkedList::push_front(DoubleLinkedListNode * node) {
+  node->next = mHead;
+  node->prev = nullptr;
+  if (mHead)
+    mHead->prev = node;
+  mHead = node;
 }
 
-DoubleLinkedListNode * DoubleLinkedListNode::next() {
-   return mNext;
+bool DoubleLinkedList::remove(DoubleLinkedListNode * node) {
+  DoubleLinkedListNode * cur = mHead;
+  //find the node
+  while (cur && cur != node)
+    cur = cur->next;
+  if (!cur) //not found
+    return false;
+
+  //fix up the list
+  if (cur->next)
+    cur->next->prev = cur->prev;
+  if (cur->prev)
+    cur->prev->next = cur->next;
+  if (cur == mHead)
+    mHead = cur->next;
+  cur->next = nullptr;
+  cur->prev = nullptr;
+  return true;
 }
 
-DoubleLinkedListNode * DoubleLinkedListNode::prev() {
-   return mPrev;
-}
-
-bool DoubleLinkedListNode::insert_before(DoubleLinkedListNode * node) {
-
-   //if we have a node behind us, just insert the node after that
-   if (mPrev) {
-      mPrev->insert_after(node);
-      return false;
-   }
-
-   //otherwise set the node's pointers and our prev pointer
-   node->mNext = this;
-   node->mPrev = NULL;
-   mPrev = node;
-
-   return true;
-}
-
-bool DoubleLinkedListNode::insert_after(DoubleLinkedListNode * node) {
-   bool tail = last();
-
-   //set the nodes pointers
-   node->mNext = mNext;
-   node->mPrev = this;
-   //if we have a next node, set its prev pointer
-   if (mNext)
-      mNext->mPrev = node;
-   //set our new next pointer
-   mNext = node;
-
-   return tail;
-}
-
-DoubleLinkedListNode * DoubleLinkedListNode::remove() {
-   DoubleLinkedListNode * ret = mPrev;
-
-   //if there is a next node set its prev node to our prev node
-   if (mNext)
-      mNext->mPrev = mPrev;
-
-   //if there is a prev node, set its next node to our next node
-   //otherwise, set our return value to our next node
-   if (mPrev)
-      mPrev->mNext = mNext;
-   else
-      ret = mNext;
-
-   //clear out our pointers
-   mPrev = NULL;
-   mNext = NULL;
-   return ret;
+void DoubleLinkedList::each(std::function<void(DoubleLinkedListNode * node)> func) {
+  DoubleLinkedListNode * cur = mHead;
+  while (cur) {
+    func(cur);
+    cur = cur->next;
+  }
 }
 
--- app/audio/doublelinkedlist.hpp
+++ app/audio/doublelinkedlist.hpp
@@ -1,34 +1,35 @@
 #ifndef DOUBLE_LINKED_LIST_HPP
 #define DOUBLE_LINKED_LIST_HPP
 
+#include <functional>
+
+class DoubleLinkedListNode;
+
+class DoubleLinkedList {
+  public:
+    DoubleLinkedList();
+    ~DoubleLinkedList();
+
+    //DoubleLinkedList takes ownership of node
+    void push_back(DoubleLinkedListNode * node);
+    void push_front(DoubleLinkedListNode * node);
+
+    //returns true when found and removed, ownership ditched
+    bool remove(DoubleLinkedListNode * node);
+
+    void each(std::function<void(DoubleLinkedListNode * node)> func);
+  private:
+    DoubleLinkedListNode * mHead = nullptr;
+};
+
 class DoubleLinkedListNode {
    public:
       DoubleLinkedListNode();
       virtual ~DoubleLinkedListNode();
-
-      //is this the first node in the list?
-      bool first();
-      //is this the last node in the list?
-      bool last();
-
-      //return the next node in the list, can be NULL
-      DoubleLinkedListNode * next();
-      //return the previous node in the list, can be NULL
-      DoubleLinkedListNode * prev();
-
-      //returns true if the newly inserted node is now the head of the list
-      //returns false otherwise
-      bool insert_before(DoubleLinkedListNode * node);
-
-      //returns true if the newly inserted node is now the tail of the list
-      //returns false otherwise
-      bool insert_after(DoubleLinkedListNode * node);
-
-      //returns the previous node.. unless it is the first node, then it returns the next node
-      DoubleLinkedListNode * remove();
    private:
-      DoubleLinkedListNode * mNext;
-      DoubleLinkedListNode * mPrev;
+      friend class DoubleLinkedList;
+      DoubleLinkedListNode * next = nullptr;
+      DoubleLinkedListNode * prev = nullptr;
 };
 
 #endif
