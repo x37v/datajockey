dd97b7c61b3b5c778d6858f1cc7107cd045ddf03
Alex Norman <x37v.alex@gmail.com>
2015-03-24 21:24:46 -0700

    session playlist
    script to dump a playlist after a session

---
+++ ruby/scripts/session_playlist.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+=begin
+	This file is part of Data Jockey.
+	
+	Data Jockey is free software: you can redistribute it and/or modify it
+	under the terms of the GNU General Public License as published by the
+	Free Software Foundation, either version 3 of the License, or (at your
+	option) any later version.
+	
+	Data Jockey is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+	Public License for more details.
+	
+	You should have received a copy of the GNU General Public License along
+	with Data Jockey.  If not, see <http://www.gnu.org/licenses/>.
+=end
+
+SESSION = 126
+
+$: << ".."
+
+require 'datajockey/base'
+require 'datajockey/db'
+require 'yaml'
+require_relative 'getsamplerate'
+
+#connect to the database
+Datajockey::connect
+
+include Datajockey
+
+AudioWorkHistory.where(:session_id => SESSION).order(:played_at).each do |h|
+  w = h.audio_work
+  puts "#{w.artist.name}: '#{w.name}'"
+end
+
